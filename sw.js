f3854aabe6d423860b0d996672a1133a20cd2102
almibarcurico-ai <almibarcurico@gmail.com>
2026-04-19 22:55:04 -0400

    force cache bust: sw v14

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='almibar-v13';
+var CACHE='almibar-v14';
 self.addEventListener('install',function(e){self.skipWaiting()});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(n){return Promise.all(n.map(function(k){return caches.delete(k)}))}).then(function(){return self.clients.claim()}))});
 self.addEventListener('fetch',function(e){e.respondWith(fetch(e.request).catch(function(){return caches.match(e.request)}))});
